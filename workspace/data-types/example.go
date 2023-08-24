d1120d5ff4b127f71dd4187d4d400d601e0aa1a4
Nisha <nisha.v@ibm.com>
2023-08-24 19:57:03 +0530

    go data types

---
+++ workspace/data-types/example.go
@@ -0,0 +1,16 @@
+package datatypes
+
+import (
+	"fmt"
+)
+
+func init() {
+	fmt.Println("data types example")
+}
+
+func intigerSum() {
+	var a int16 = 300
+	var b int16 = 200
+	var sum int16 = a + b
+	fmt.Println("sum of integers ", sum)
+}
